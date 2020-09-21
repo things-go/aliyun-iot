afbb028bfe0b9404eb599360a5ce245beba40ee1
mo <thinkgo@aliyun.com>
2020-09-21 10:29:44 +0800

    fix test

--- _examples/mock/mock.go
+++ _examples/mock/mock.go
@@ -1,15 +1,28 @@
 package mock
 
 import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+	"runtime"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/thinkgos/go-core-package/lib/logger"
+
+	aiot "github.com/thinkgos/aliyun-iot"
+	"github.com/thinkgos/aliyun-iot/dm"
 	"github.com/thinkgos/aliyun-iot/infra"
+	"github.com/thinkgos/aliyun-iot/sign"
 )
 
 // just for test
 const (
-	ProductKey    = "a1QR3GD1Db3"
-	ProductSecret = "mvngTYBlX9Z9l1V0"
-	DeviceName    = "dynamic"
-	DeviceSecret  = "9690f9da431078f105b7969b23e05762"
+	ProductKey    = "a1NHTWLlMny"
+	ProductSecret = "qv30SFtpGf3tSBfP"
+	DeviceName    = "mygateway"
+	DeviceSecret  = "723ff3e4a4352d09a971171b6d52a1eb"
 )
 
 var MetaTetrad = infra.MetaTetrad{
@@ -26,11 +39,94 @@ var MetaTriad = infra.MetaTriad{
 }
 
 type Instance struct {
-	Temp         int `json:"temp"`
-	Humi         int `json:"humi"`
-	SwitchStatus int `json:"switchStatus"`
+	GatewayVersion string  `json:"gateway_version"`
+	SystemInfo     string  `json:"system_info"`
+	CpuUsage       float32 `json:"cpu_usage"`
+	MemoryUsage    float32 `json:"memory_usage"`
+	MemoryTotal    float64 `json:"memory_total"`
+	MemoryFree     float64 `json:"memory_free"`
+	CpuCoreNumber  int32   `json:"cpu_core_number"`
+	DiskUsage      float32 `json:"disk_usage"`
+	LightSwitch    int     `json:"light_switch"`
 }
 
 type Alarm struct {
 	High int `json:"high"`
+}
+
+func Init() *aiot.MQTTClient {
+	signs, err := sign.Generate(MetaTriad, infra.CloudRegionDomain{Region: infra.CloudRegionShangHai})
+	if err != nil {
+		panic(err)
+	}
+	opts :=
+		mqtt.NewClientOptions().
+			AddBroker(signs.Addr()).
+			SetClientID(signs.ClientIDWithExt()).
+			SetUsername(signs.UserName).
+			SetPassword(signs.Password).
+			SetCleanSession(true).
+			SetAutoReconnect(true).
+			SetOnConnectHandler(func(cli mqtt.Client) {
+				log.Println("mqtt client connection success")
+			}).
+			SetConnectionLostHandler(func(cli mqtt.Client, err error) {
+				log.Println("mqtt client connection lost, ", err)
+			})
+
+	client := aiot.NewWithMQTT(
+		MetaTriad,
+		mqtt.NewClient(opts),
+		dm.WithEnableNTP(),
+		dm.WithEnableDesired(),
+		dm.WithCallback(mockCb{}),
+		dm.WithLogger(logger.New(log.New(os.Stdout, "mqtt --> ", log.LstdFlags), logger.WithEnable(true))),
+	)
+
+	client.Underlying().Connect().Wait()
+	if err = client.Connect(); err != nil {
+		panic(err)
+	}
+	return client
+}
+
+type mockCb struct {
+	dm.NopCb
+}
+
+func (sf mockCb) RRPCRequest(c *dm.Client, messageID, productKey, deviceName string, payload []byte) error {
+	req := &dm.Request{}
+	if err := json.Unmarshal(payload, req); err != nil {
+		return err
+	}
+	c.Log.Debugf("rrpc.resopnse.%s", messageID)
+	c.Log.Debugf("%+v", req)
+	return c.RRPCResponse(productKey, deviceName, messageID, dm.Response{
+		ID:   req.ID,
+		Code: infra.CodeSuccess,
+		Data: "{}",
+	})
+}
+
+func ThingEventPropertyPost(client *aiot.MQTTClient) {
+	for {
+		time.Sleep(time.Second * 30)
+		stats := runtime.MemStats{}
+		runtime.ReadMemStats(&stats)
+		err := client.LinkThingEventPropertyPost(ProductKey, DeviceName,
+			Instance{
+				GatewayVersion: "v0.1.0",
+				SystemInfo:     "test gateway",
+				CpuUsage:       float32(rand.Intn(1000)) / 10,
+				MemoryUsage:    float32(stats.HeapInuse / stats.HeapSys),
+				MemoryTotal:    float64(stats.HeapSys),
+				MemoryFree:     float64(stats.HeapIdle),
+				CpuCoreNumber:  int32(runtime.NumCPU()),
+				DiskUsage:      float32(rand.Intn(1000)) / 10,
+				LightSwitch:    rand.Intn(2),
+			}, time.Second)
+		if err != nil {
+			log.Printf("error: %#v", err)
+		}
+	}
 }
--- _examples/mqtt_dm/mqtt_dm.go
+++ _examples/mqtt_dm/mqtt_dm.go
@@ -1,89 +1,40 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"math/rand"
-	"os"
 	"time"
-
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/thinkgos/go-core-package/lib/logger"
 
 	aiot "github.com/thinkgos/aliyun-iot"
 	"github.com/thinkgos/aliyun-iot/_examples/mock"
 	"github.com/thinkgos/aliyun-iot/dm"
 	"github.com/thinkgos/aliyun-iot/dmd"
 	"github.com/thinkgos/aliyun-iot/infra"
-	"github.com/thinkgos/aliyun-iot/sign"
 )
 
-var dmClient *aiot.MQTTClient
-
 func main() {
-	signs, err := sign.Generate(mock.MetaTriad, infra.CloudRegionDomain{Region: infra.CloudRegionShangHai})
-	if err != nil {
-		panic(err)
-	}
-	opts :=
-		mqtt.NewClientOptions().
-			AddBroker(signs.Addr()).
-			SetClientID(signs.ClientIDWithExt()).
-			SetUsername(signs.UserName).
-			SetPassword(signs.Password).
-			SetCleanSession(true).
-			SetAutoReconnect(true).
-			SetOnConnectHandler(func(cli mqtt.Client) {
-				log.Println("mqtt client connection success")
-			}).
-			SetConnectionLostHandler(func(cli mqtt.Client, err error) {
-				log.Println("mqtt client connection lost, ", err)
-			})
+	client := mock.Init()
 
-	dmClient = aiot.NewWithMQTT(
-		mock.MetaTriad,
-		mqtt.NewClient(opts),
-		dm.WithEnableNTP(),
-		dm.WithEnableDesired(),
-		dm.WithCallback(mockCb{}),
-		dm.WithLogger(logger.New(log.New(os.Stdout, "mqtt --> ", log.LstdFlags), logger.WithEnable(true))),
-	)
+	// go DiagPostTest(client) // done
+	// go ConfigLogGetTest(client) // done
+	// go dynamictslTest(client)
+	// go DslTemplateTest(client) // done
+	// go DesiredGetTest(client) // done
+	// go DesiredDeleteTest(client) // done
+	// go ConfigTest(client) // done
+	go NTPTest(client) // done
+	// go DeviceInfoTest(client) // done
+	// go ThingEventPost(client) // done
 
-	dmClient.Underlying().Connect().Wait()
-	if err = dmClient.Connect(); err != nil {
-		panic(err)
-	}
-
-	// go DiagPostTest() // done
-	// go ConfigLogGetTest() // done
-	// go dynamictslTest()
-	// go DslTemplateTest() // done
-	// go DesiredGetTest() // done
-	// go DesiredDeleteTest() // done
-	// go ConfigTest() // done
-	// go NTPTest() // done
-	// go DeviceInfoTest() // done
-	// go ThingEventPost() // done
-	for {
-		time.Sleep(time.Second * 30)
-		err := dmClient.LinkThingEventPropertyPost(mock.ProductKey, mock.DeviceName,
-			mock.Instance{
-				rand.Intn(200),
-				rand.Intn(100),
-				rand.Intn(2),
-			}, time.Second)
-		if err != nil {
-			log.Printf("error: %#v", err)
-		}
-	}
+	mock.ThingEventPropertyPost(client)
 }
 
 // done
-func ThingEventPost() {
+func ThingEventPost(client *aiot.MQTTClient) {
 	for {
-		err := dmClient.LinkThingEventPost(mock.ProductKey, mock.DeviceName, "tempAlarm",
-			map[string]interface{}{
-				"high": 1,
+		err := client.LinkThingEventPost(mock.ProductKey, mock.DeviceName, "alarm",
+			mock.Alarm{
+				rand.Intn(2),
 			}, time.Second*5)
 		if err != nil {
 			log.Printf("error: %#v", err)
@@ -93,8 +44,8 @@ func ThingEventPost() {
 }
 
 // done
-func DeviceInfoTest() {
-	err := dmClient.LinkThingDeviceInfoUpdate(mock.ProductKey, mock.DeviceName,
+func DeviceInfoTest(client *aiot.MQTTClient) {
+	err := client.LinkThingDeviceInfoUpdate(mock.ProductKey, mock.DeviceName,
 		[]dmd.DeviceInfoLabel{
 			{AttrKey: "attrKey", AttrValue: "attrValue"},
 		}, time.Second*5)
@@ -103,7 +54,7 @@ func DeviceInfoTest() {
 		return
 	}
 	time.Sleep(time.Minute * 1)
-	err = dmClient.LinkThingDeviceInfoDelete(mock.ProductKey, mock.DeviceName,
+	err = client.LinkThingDeviceInfoDelete(mock.ProductKey, mock.DeviceName,
 		[]dmd.DeviceLabelKey{{AttrKey: "attrKey"}}, time.Second*5)
 	if err != nil {
 		log.Println(err)
@@ -112,8 +63,8 @@ func DeviceInfoTest() {
 }
 
 // done
-func ConfigTest() {
-	cpd, err := dmClient.LinkThingConfigGet(mock.ProductKey, mock.DeviceName, time.Second*5)
+func ConfigTest(client *aiot.MQTTClient) {
+	cpd, err := client.LinkThingConfigGet(mock.ProductKey, mock.DeviceName, time.Second*5)
 	if err != nil {
 		log.Println(err)
 		return
@@ -121,8 +72,8 @@ func ConfigTest() {
 	log.Printf("config: %+v", cpd)
 }
 
-func DslTemplateTest() {
-	data, err := dmClient.LinkThingDsltemplateGet(mock.ProductKey, mock.DeviceName, time.Second*5)
+func DslTemplateTest(client *aiot.MQTTClient) {
+	data, err := client.LinkThingDsltemplateGet(mock.ProductKey, mock.DeviceName, time.Second*5)
 	if err != nil {
 		log.Println(err)
 		return
@@ -130,24 +81,24 @@ func DslTemplateTest() {
 	log.Printf("%+v", string(data))
 }
 
-func dynamictslTest() {
-	_, err := dmClient.LinkThingDynamictslGet(mock.ProductKey, mock.DeviceName, time.Second*5)
+func dynamictslTest(client *aiot.MQTTClient) {
+	_, err := client.LinkThingDynamictslGet(mock.ProductKey, mock.DeviceName, time.Second*5)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
 // done
-func NTPTest() {
-	err := dmClient.ExtNtpRequest()
+func NTPTest(client *aiot.MQTTClient) {
+	err := client.ExtNtpRequest()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 }
 
-func DesiredGetTest() {
-	data, err := dmClient.LinkThingDesiredPropertyGet(mock.ProductKey, mock.DeviceName, []string{"temp", "humi"}, time.Second*5)
+func DesiredGetTest(client *aiot.MQTTClient) {
+	data, err := client.LinkThingDesiredPropertyGet(mock.ProductKey, mock.DeviceName, []string{"temp", "humi"}, time.Second*5)
 	if err != nil {
 		log.Println(err)
 		return
@@ -155,16 +106,16 @@ func DesiredGetTest() {
 	log.Printf("%+v", string(data))
 }
 
-func DesiredDeleteTest() {
-	err := dmClient.LinkThingDesiredPropertyDelete(mock.ProductKey, mock.DeviceName, "{}", time.Second*5)
+func DesiredDeleteTest(client *aiot.MQTTClient) {
+	err := client.LinkThingDesiredPropertyDelete(mock.ProductKey, mock.DeviceName, "{}", time.Second*5)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 }
 
-func ConfigLogGetTest() {
-	data, err := dmClient.LinkThingConfigLogGet(mock.ProductKey, mock.DeviceName, dm.ConfigLogParam{}, time.Second*5)
+func ConfigLogGetTest(client *aiot.MQTTClient) {
+	data, err := client.LinkThingConfigLogGet(mock.ProductKey, mock.DeviceName, dm.ConfigLogParam{}, time.Second*5)
 	if err != nil {
 		log.Println(err)
 		return
@@ -172,8 +123,8 @@ func ConfigLogGetTest() {
 	log.Printf("%+v", data)
 }
 
-func DiagPostTest() {
-	err := dmClient.LinkThingDiagPost(mock.ProductKey, mock.DeviceName, dm.P{
+func DiagPostTest(client *aiot.MQTTClient) {
+	err := client.LinkThingDiagPost(mock.ProductKey, mock.DeviceName, dm.P{
 		Wifi: dm.Wifi{
 			Rssi:     100,
 			Snr:      10,
@@ -186,22 +137,4 @@ func DiagPostTest() {
 		log.Println(err)
 		return
 	}
-}
-
-type mockCb struct {
-	dm.NopCb
-}
-
-func (sf mockCb) RRPCRequest(c *dm.Client, messageID, productKey, deviceName string, payload []byte) error {
-	req := &dm.Request{}
-	if err := json.Unmarshal(payload, req); err != nil {
-		return err
-	}
-	c.Log.Debugf("rrpc.resopnse.%s", messageID)
-	c.Log.Debugf("%+v", req)
-	return c.RRPCResponse(productKey, deviceName, messageID, dm.Response{
-		ID:   req.ID,
-		Code: infra.CodeSuccess,
-		Data: "{}",
-	})
 }
